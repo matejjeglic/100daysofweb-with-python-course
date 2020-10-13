f2cff927e9fdee8321e5c291830f61f70ff6e0f4
Matej <matej.jeglic@gmail.com>
2020-10-13 11:11:11 +0200

    added chuck noris joke page

--- days/001-004-flask-intro/code/program/routes.py
+++ days/001-004-flask-intro/code/program/routes.py
@@ -1,11 +1,27 @@
+from datetime import datetime
+import requests
 from flask import render_template
 from program import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
+    timenow = str(datetime.now())
     return render_template('index.html', title='Template Demo', time=timenow)
+
 
 @app.route('/100Days')
 def p100days():
     return render_template('100Days.html')
+
+
+def get_chuck_joke():
+    r = requests.get('https://api.chucknorris.io/jokes/random')
+    data = r.json()
+    return data['value']
+
+
+@app.route('/chuck')
+def chuck():
+    return render_template('chuck.html', joke=get_chuck_joke())
